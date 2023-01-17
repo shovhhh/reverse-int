b111f222fe04b24087b590b2c4317a77cb42f5e1
shovhhh <vip.shorokhov@mail.ru>
2023-01-17 22:56:26 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+   let result = '';
+   let strN = String(n);
+   if (strN[0] !== '-') {
+      for (let i = 0; i < strN.length; i += 1) {
+         result = `${strN[i]}${result}`;
+      }
+   } else if (strN[0] === '-') {
+      for (let i = 1; i < strN.length; i += 1) {
+         result = `${strN[i]}${result}`;
+      }
+   }
+   return Number(result);
 }
